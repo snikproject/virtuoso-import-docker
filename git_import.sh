77098179bf18139dc0ce4d25e8d0cfd7acdf1369
Natanael Arndt <arndtn@gmail.com>
2020-08-26 19:21:34 +0200

    fix indents

--- git_import.sh
+++ git_import.sh
@@ -4,7 +4,7 @@
 if [ -z "$GIT_REPO" ]; then
 	:
 	#data is in import volume
-    echo "[ERROR] git repo not given. Stop here."
+	echo "[ERROR] git repo not given. Stop here."
 else
 
 	cd $VIRTUOSO_DATA_DIR
@@ -14,14 +14,14 @@ else
 	status=$?
 
 	if [ $status -ne 0 ]; then
-	    echo "[INFO] Repository is not a git directory. Clone now."
+			echo "[INFO] Repository is not a git directory. Clone now."
 			if [ -n "$(ls -A $VIRTUOSO_DATA_DIR)" ]; then
 				echo "[ERROR] Target directory is not empty. Can't clone."
 			fi
 			git clone $GIT_REPO $VIRTUOSO_DATA_DIR
 	else
-	    echo "[INFO] Update repository ..."
-	    git pull
+		echo "[INFO] Update repository ..."
+		git pull
 	fi
 
 	echo "[INFO] git repo cloned. Continue with import."
