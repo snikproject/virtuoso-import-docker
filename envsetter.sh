503f91c14e06f48bc6769a169cde611693abceed
Natanael Arndt <arndtn@gmail.com>
2021-02-15 18:37:23 +0100

    Install crontab

--- envsetter.sh
+++ envsetter.sh
@@ -2,6 +2,7 @@
 
 if [ -n "$CRON_TIMES" ]; then
   sed -i "s/1,6,11,16,21,26,31,36,41,46,51,56 \* \* \* \*/$CRON_TIMES/" /etc/cron.d/cronfile
+  crontab /etc/cron.d/cronfile # install new crontab
 fi
 
 printenv >> /etc/environment
